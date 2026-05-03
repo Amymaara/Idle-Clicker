6e4acd76f2fdaa415bd818419b8ff3f51ed6017f
Amymaara <98781672+Amymaara@users.noreply.github.com>
2026-05-03 18:06:05 +0200

    base charm system

--- My project/Assets/Scripts/PanelManager.cs
+++ My project/Assets/Scripts/PanelManager.cs
@@ -2,15 +2,60 @@ using UnityEngine;
 
 public class PanelManager : MonoBehaviour
 {
-    // Start is called once before the first execution of Update after the MonoBehaviour is created
-    void Start()
+
+    [SerializeField] private CanvasGroup mainPanel;
+    [SerializeField] private CanvasGroup upgradePanel;
+    [SerializeField] private CanvasGroup apprenticePanel;
+    [SerializeField] private CanvasGroup charmPanel;
+
+    private void Start()
     {
-        
+        ShowMain();
     }
 
-    // Update is called once per frame
-    void Update()
+    public void ShowMain()
     {
-        
+        ShowPanel(mainPanel);
+        HidePanel(upgradePanel);
+        HidePanel(apprenticePanel);
+        HidePanel(charmPanel);
+    }
+
+    public void ShowUpgrade()
+    {
+        HidePanel(mainPanel);
+        ShowPanel(upgradePanel);
+        HidePanel(apprenticePanel);
+        HidePanel(charmPanel);
+    }
+
+    public void ShowApprentice()
+    {
+        HidePanel(mainPanel);
+        HidePanel(upgradePanel);
+        ShowPanel(apprenticePanel);
+        HidePanel(charmPanel);
+    }
+
+    public void ShowCharms()
+    {
+        HidePanel(mainPanel);
+        HidePanel(upgradePanel);
+        HidePanel(apprenticePanel);
+        ShowPanel(charmPanel);
+    }
+
+    private void ShowPanel(CanvasGroup panel)
+    {
+        panel.alpha = 1;
+        panel.interactable = true;
+        panel.blocksRaycasts = true;
+    }
+
+    private void HidePanel(CanvasGroup panel)
+    {
+        panel.alpha = 0;
+        panel.interactable = false;
+        panel.blocksRaycasts = false;
     }
 }
