d08bf63720ad106462265ffa2e732de9d4844598
Amymaara <98781672+Amymaara@users.noreply.github.com>
2026-05-06 19:30:57 +0200

    final form reached

--- My project/Assets/Scripts/NumberFormatter.cs
+++ My project/Assets/Scripts/NumberFormatter.cs
@@ -2,16 +2,22 @@ public static class NumberFormatter
 {
     public static string FormatMoney(double value)
     {
-        if (value >= 1_000_000_000_000)
+        if (value >= 1_000_000_000_000_000_000d)
+            return "$" + (value / 1_000_000_000_000_000_000d).ToString("0.##") + "Qi";
+
+        if (value >= 1_000_000_000_000_000d)
+            return "$" + (value / 1_000_000_000_000_000d).ToString("0.##") + "Qa";
+
+        if (value >= 1_000_000_000_000d)
             return "$" + (value / 1_000_000_000_000d).ToString("0.##") + "T";
 
-        if (value >= 1_000_000_000)
+        if (value >= 1_000_000_000d)
             return "$" + (value / 1_000_000_000d).ToString("0.##") + "B";
 
-        if (value >= 1_000_000)
+        if (value >= 1_000_000d)
             return "$" + (value / 1_000_000d).ToString("0.##") + "M";
 
-        if (value >= 1_000)
+        if (value >= 1_000d)
             return "$" + (value / 1_000d).ToString("0.##") + "K";
 
         return "$" + value.ToString("F0");
